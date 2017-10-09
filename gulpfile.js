68cd108496ec4a4be9d54a398846f235da144837
Keven Jesus <falecom@keven.com.br>
2017-10-09 13:36:12 -0300

    task duplicada

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
 var gulp = require("gulp");
 var htmlmin = require('gulp-htmlmin');
 var sass = require("gulp-sass");
-var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
-gulp.task('compila-move-css', function(){
-    gulp.src('./source/scss/style.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/css'));
-});
+
+
 gulp.task('minify-css', function(){
     gulp.src('./source/scss/style.scss')
     .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(cssmin())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./dist/css'));
 });
